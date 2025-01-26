3b3a400ffb1a5df66c50bec2639062a2fb6ee6a6
f14amy <f14mygd@gmail.com>
2025-01-27 00:25:05 +0300

    Тарифы
    Переделывал 3 раза из за проблем с дизайном я бездарь

--- src/routes/tarifs/+page.svelte
+++ src/routes/tarifs/+page.svelte
@@ -1,27 +1,302 @@
 <script>
     import Header from "../header.svelte";
-
 </script>
 
 <section>
     <Header></Header>
     <div class="Main">
-        <div class="MainText">Тарифы</div>
+            <div class="MainText">Тарифы</div>
+        <div class="TarifsView">
+            <div class="Tarif" style="background-color: white;">
+                <div class="TarifName">Start</div>
+                <div class="TarifDesc">Выбор для игры с друзьями на пару раз, или теста нашего хостинга!</div>
+                <div class="TarifServ">
+                    <div class="TarifPunct" style="margin-top: 20px; padding-bottom: 5px;">
+                        <div class="TPD">Услуга</div>
+                        <div class="TPCС">Доступность</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Удобная панель управления</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Плагины</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Моды</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Сборщик 2.2</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Дискорд/телеграм боты</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Свои главные уровни</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Музыка только newgrounds</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Ресурс паки</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Реклама</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Магазин</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Сайт</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                </div>
+                <button class="TarifButton">24₽/мес</button>
+            </div>
+            <div class="AdaptiveHelper">
+            <div class="Tarif">
+                <div class="TarifName">Medium</div>
+                <div class="TarifDesc">Популярный выбор для маленьких и средних серверов, не много и не мало!</div>
+                <div class="TarifServ">
+                    <div class="TarifPunct" style="margin-top: 20px; padding-bottom: 5px;">
+                        <div class="TPD">Услуга</div>
+                        <div class="TPCС">Доступность</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Удобная панель управления</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Плагины</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Моды</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Сборщик 2.1 2.2</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Дискорд/телеграм боты</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Свои главные уровни</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Любая музыка</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Ресурс паки</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Реклама</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Магазин</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Сайт</div>
+                        <div class="TPC" style="color: #FF595C;">×</div>
+                    </div>
+                </div>
+                <button class="TarifButton">99₽/мес</button>
+            </div>
+            <div class="Tarif" style="background-color: white;">
+                <div class="TarifName">Fullstack</div>
+                <div class="TarifDesc">Выбор для крупных серверов с большими возможностями!</div>
+                <div class="TarifServ">
+                    <div class="TarifPunct" style="margin-top: 20px; padding-bottom: 5px;">
+                        <div class="TPD">Услуга</div>
+                        <div class="TPCС">Доступность</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Удобная панель управления</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Плагины</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Моды</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Сборщик 1.9 2.1 2.2</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Дискорд/телеграм боты</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Свои главные уровни</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Любая музыка</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Ресурс паки</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Реклама</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Магазин</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                    <div class="TarifPunct">
+                        <div class="TPD">Сайт</div>
+                        <div class="TPC" style="color: #00FF84;">✔</div>
+                    </div>
+                </div>
+                <button class="TarifButton">199₽/мес</button>
+            </div>
+        </div>
+        </div>
     </div>
 </section>
 
 <style>
+
+    .TarifButton {
+        width: 160px;
+        height: 40px;
+        font-family: "Inter", serif;
+        font-size: 14px;
+        font-weight: 300;
+        color: #ffffff;
+        background-color: #5e74ff;
+        border: none;
+        border-radius: 100px;
+        transition: 0.5s;
+        margin-top: 25px;
+    }
+
+    .TarifButton:hover {
+        background-color: #3550ff;
+    }
+
+    .TPCС {
+        font-size: 16px;
+        font-weight: 300;
+        font-family: "Inter", serif;
+        display: flex;
+        align-items: baseline;
+    }
+
+    .TPC {
+        font-size: 18px;
+        font-weight: 300;
+        font-family: "Inter", serif;
+        display: flex;
+        align-items: baseline;
+    }
+
+    .TPD {
+        display: flex;
+        font-family: "Inter", serif;
+        font-size: 16px;
+        font-weight: 300;
+        display: flex;
+    }
+
+    .TarifPunct {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 350px;
+        border-bottom: 2px solid #e4e4e4;
+        margin-top: 5px;
+        height: 23px;
+    }
+
+    .TarifDesc {
+        display: flex;
+        font-family: "Inter", serif;
+        font-size: 16px;
+        font-weight: 300;
+        text-align: center;
+        margin-top: 13px;
+    }
+
+
+    .TarifName {
+        display: flex;
+        font-family: "Inter", serif;
+        font-size: 28px;
+        font-weight: 400;
+    }
+
+    .TarifsView {
+        display: flex;
+        flex-direction: row;
+        margin-top: 20px;
+        align-items: center;
+    }
+
+    .Tarif {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        height: 525px;
+        width: 350px;
+        background-color: #F9F9F9;
+        padding: 20px;
+        margin-left: 10px;
+    }
+
     .Main {
         display: flex;
         justify-content: center;
-        margin-top: 10vh;
+        margin-top: 3vh;
         flex-direction: column;
         align-items: center;
     }
 
     .MainText {
+        display: flex;
         font-family: "Inter", serif;
         font-size: 34px;
         font-weight: 300;
     }
+
+    .AdaptiveHelper {
+        display: flex;
+        flex-direction: row;
+    }
+
+    @media screen and (max-width: 820px) {
+    .TarifsView {
+        flex-direction: column;
+    }
+    }
+
+    @media screen and (max-width: 1200px) {
+    .AdaptiveHelper {
+        flex-direction: column;
+    }
+    }
+
 </style>
